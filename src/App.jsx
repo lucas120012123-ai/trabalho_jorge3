fe80bfda41cf9636e638955680c726c878fa7f6d
lucas120012123-ai <lucas120012123@gmail.com>
2025-12-04 17:05:02 -0300

    Correção do erro "loga-vite"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import viteLogo from '/vite.svg'
 import Navbar from "./navbar/navbar";
 import CaixaDeMensagem from "./componentes/CaixaDeMensagem";
 import './App.css'
